18329c4b677379e89f4ec22804dee5d40cdd6ef3
Hugo Dettner Källander <hugokallander@gmail.com>
2024-11-10 20:04:58 +0100

    Add Relational improvements

---
+++ src/Relational.ts
@@ -0,0 +1,87 @@
+abstract class Relational<RelObj extends Object, RelEnum> {
+    relationLookup: { [key: string]: Set<[RelObj, RelObj]> } = {};
+
+    constructor(...enumTypes: RelEnum[]) {
+        const types = enumTypes.filter(v => typeof v === 'string');
+        types.forEach((type1, index1) => {
+            types.forEach((type2, index2) => {
+                if (index1 !== index2) {
+                    const key = `${type1},${type2}`;
+                    this.relationLookup[key] = new Set<[RelObj, RelObj]>();
+                }
+            });
+        });
+    }
+
+    protected getTypeRelations(type1: String, type2: String): Set<[RelObj, RelObj]> {
+        const key = `${type1},${type2}`;
+        const keyRev = `${type2},${type1}`;
+        return this.relationLookup[key] || this.relationLookup[keyRev] || new Set();
+    }
+
+    protected getObjRelations(obj1: RelObj, obj2: RelObj): Set<[RelObj, RelObj]> {
+        return this.getTypeRelations(obj1.constructor.name, obj2.constructor.name);
+    }
+
+    protected getRelatedObjs(obj: RelObj, relEnum: RelEnum): Set<RelObj> {
+        const relatedObjs = new Set<RelObj>();
+        const relations = this.getTypeRelations(obj.constructor.name, String(relEnum));
+        relations.forEach(([o1, o2]) => {
+            if (o1 === obj) relatedObjs.add(o2);
+            if (o2 === obj) relatedObjs.add(o1);
+        });
+        return relatedObjs;
+    }
+
+    protected getSetElement(set: Set<RelObj>): RelObj | null {
+        return set.values().next().value;
+    }
+
+    protected getRelatedObj(obj: RelObj, relEnum: RelEnum): RelObj | null {
+        const relatedObjs = this.getRelatedObjs(obj, relEnum);
+        return this.getSetElement(relatedObjs);
+    }
+
+    protected getRelation(obj1: RelObj, obj2: RelObj): Set<[RelObj, RelObj]> {
+        const relation = this.getObjRelations(obj1, obj2);
+        const filteredRelations = new Set<[RelObj, RelObj]>(
+            Array.from(relation).filter(([o1, o2]) => o1 === obj1 && o2 === obj2)
+        );
+        return filteredRelations;
+    }
+
+    protected hasRelation(obj1: RelObj, obj2: RelObj): boolean {
+        return this.getRelation(obj1, obj2).size > 0;
+    }
+    
+    protected getConnectors(obj1: RelObj, middle: Set<RelObj>, obj2: RelObj): Set<RelObj> {
+        const connectors = new Set<RelObj>();
+        middle.forEach(m => {
+            if (this.hasRelation(obj1, m) && this.hasRelation(m, obj2)) connectors.add(m);
+        });
+        return connectors;
+    }
+
+    protected getIndirectObjs(obj1: RelObj, relEnum1: RelEnum, relEnum2: RelEnum): Set<RelObj> {
+        const middleObjs = this.getRelatedObjs(obj1, relEnum1);
+        const indirectObjs = new Set<RelObj>();
+        middleObjs.forEach(m => {
+            const relatedObjs = this.getRelatedObjs(m, relEnum2);
+            relatedObjs.forEach(o => indirectObjs.add(o));
+        });
+
+        return indirectObjs;
+    }
+
+    protected hasConnection(obj1: RelObj, middle: Set<RelObj>, obj2: RelObj): boolean {
+        return this.getConnectors(obj1, middle, obj2).size > 0;
+    }
+
+    protected addRelation(obj1: RelObj, obj2: RelObj) {
+        this.getObjRelations(obj1, obj2).add([obj1, obj2]);
+    }
+
+    protected removeRelation(obj1: RelObj, obj2: RelObj) {
+        this.getObjRelations(obj1, obj2).delete([obj1, obj2]);
+    }
+}
--- src/game.ts
+++ src/game.ts
@@ -1,98 +1,18 @@
-abstract class Relational<RelObj, Relation extends [RelObj, RelObj], RelEnum> {
-    relationLookup: { [key: string]: Set<Relation> } = {};
-
-    protected getRelations(obj1: RelObj, obj2: RelObj): Set<Relation> {
-        const key = `${obj1.constructor.name},${obj2.constructor.name}`;
-        const keyRev = `${obj2.constructor.name},${obj1.constructor.name}`;
-        return this.relationLookup[key] || this.relationLookup[keyRev] || new Set();
-    }
-
-    protected getRelatedObjs(obj: RelObj, relations: Set<Relation>): Set<RelObj> {
-        const relatedObjs = new Set<RelObj>();
-        relations.forEach(([o1, o2]) => {
-            if (o1 === obj) relatedObjs.add(o2);
-            if (o2 === obj) relatedObjs.add(o1);
-        });
-        return relatedObjs;
-    }
-
-    protected getSetElement(set: Set<RelObj>): RelObj | null {
-        return set.values().next().value;
-    }
-
-    protected getRelatedObj(obj: RelObj, relations: Set<Relation>): RelObj | null {
-        const relatedObjs = this.getRelatedObjs(obj, relations);
-        return this.getSetElement(relatedObjs);
-    }
-
-    protected getRelation(obj1: RelObj, obj2: RelObj): Set<Relation> {
-        const relation = this.getRelations(obj1, obj2);
-        const filteredRelations = new Set<Relation>(
-            Array.from(relation).filter(([o1, o2]) => o1 === obj1 && o2 === obj2)
-        );
-        return filteredRelations;
-    }
-
-    protected hasRelation(obj1: RelObj, obj2: RelObj): boolean {
-        return this.getRelation(obj1, obj2).size > 0;
-    }
-    
-    protected getConnectors(obj1: RelObj, middle: Set<RelObj>, obj2: RelObj): Set<RelObj> {
-        const connectors = new Set<RelObj>();
-        middle.forEach(m => {
-            if (this.hasRelation(obj1, m) && this.hasRelation(m, obj2)) connectors.add(m);
-        });
-        return connectors;
-    }
-
-    protected getIndirectObjs(obj1: RelObj, relation1: Set<Relation>, relation2: Set<Relation>): Set<RelObj> {
-        const middleObjs = this.getRelatedObjs(obj1, relation1);
-        const indirectObjs = new Set<RelObj>();
-        middleObjs.forEach(m => {
-            const relatedObjs = this.getRelatedObjs(m, relation2);
-            relatedObjs.forEach(o => indirectObjs.add(o));
-        });
-
-        return indirectObjs;
-    }
-
-    protected hasConnection(obj1: RelObj, middle: Set<RelObj>, obj2: RelObj): boolean {
-        return this.getConnectors(obj1, middle, obj2).size > 0;
-    }
-
-    protected addRelation(obj1: RelObj, obj2: RelObj) {
-        this.getRelation(obj1, obj2).add([obj1, obj2] as Relation);
-    }
-
-    protected removeRelation(obj1: RelObj, obj2: RelObj) {
-        this.getRelation(obj1, obj2).delete([obj1, obj2] as Relation);
-    }
-}
-
-type Member = [Team, Player];
-type Taken = [Station, Player];
-type Mission = [Team, Station];
-type GameRelation = Member | Taken | Mission;
 enum G {
     Team,
     Player,
     Station
 }
 
-class Game extends Relational<GameObj, GameRelation, G> {
+class Game extends Relational<GameObj, G> {
     static instance: Game | null = null;
 
     constructor() {
-        super();
+        super(G.Team, G.Player, G.Station);
         if (Game.instance) {
             return Game.instance;
         }
 
-        this.relationLookup = {
-            'Team,Player': new Set<Member>(),
-            'Station,Player': new Set<Taken>(),
-            'Team,Station': new Set<Mission>()
-        }
         Game.instance = this;
     }
 
@@ -102,10 +22,6 @@ class Game extends Relational<GameObj, GameRelation, G> {
         }
         return Game.instance;
     }
-
-    takeStation(station: Station, player: Player) {
-        this.addRelation(station, player);
-    }
     
     getPlayerTeam(player: Player): Team {
         return this.getRelatedObj(player, G.Team) as Team;
@@ -113,14 +29,6 @@ class Game extends Relational<GameObj, GameRelation, G> {
 
     getPlayerTakenStations(player: Player): Set<Station> {
         return this.getRelatedObjs(player, G.Station) as Set<Station>;
-    }
-
-    giveMission(team: Team, station: Station) {
-        this.addRelation(team, station);
-    }
-
-    addTeamPlayer(team: Team, player: Player) {
-        this.addRelation(team, player);   
     }
 
     getTeamPlayers(team: Team): Set<Player> {
@@ -139,5 +47,17 @@ class Game extends Relational<GameObj, GameRelation, G> {
     getTeamPoints(team: Team): number {
         return Array.from(this.getTeamTakenStations(team))
             .reduce((sum, station) => sum + station.points, 0);
+    }
+
+    takeStation(station: Station, player: Player) {
+        this.addRelation(station, player);
+    }
+
+    giveMission(team: Team, station: Station) {
+        this.addRelation(team, station);
+    }
+
+    addTeamPlayer(team: Team, player: Player) {
+        this.addRelation(team, player);   
     }
 }
